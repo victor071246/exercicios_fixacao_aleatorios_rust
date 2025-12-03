fbb6cdca2e91532d8ff36f84cc2ad4b9846e3acb
Victor Felipe Dadério <daderiovictorfelipe@gmail.com>
2025-12-03 00:02:50 -0300

    Exibindo corretamente PostgreSQL

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,22 @@ mod detect;
 use crate::detect::{InfoServico, buscar_portas_automatico};
 
 
-fn main() {
+fn main(){
     let resultados = buscar_portas_automatico();
 
     for r in resultados {
         match r.info {
-            InfoServico::ServicoInexistente =>
-                println!("Porta {} → aberta, mas sem serviço", r.porta),
-            InfoServico::PostgreSQL =>
-                println!("Porta {} → PostgreSQL detectado", r.porta),
-            InfoServico::MySQL =>
-                println!("Porta {} → MySQL detectado", r.porta),
-            InfoServico::MariaDB =>
-                println!("Porta {} → MariaDB detectado", r.porta),
+            InfoServico::ServicoInexistente => 
+                println!("Porta {} -> aberta mas serviço inexistente ou inacessível", r.porta),
+            
+            InfoServico::PostgreSQL => 
+                println!("Porta {} -> PostgreSQL detectado", r.porta),
+
+            InfoServico::MySQL => 
+                println!("Porta {} -> MySQL detectado", r.porta),
+            
+            InfoServico::MariaDB => 
+                println!("Porta {} -> MariaDB detectado", r.porta)
         }
     }
 }
