f8c3abacfb064d1edaf8c63cb8957e8ce91084de
Victor Felipe Dadério <daderiovictorfelipe@gmail.com>
2025-12-04 23:55:10 -0300

    adicionado interação com MySQL

--- src/detect.rs
+++ src/detect.rs
@@ -19,7 +19,7 @@ fn porta_aberta(porta: u16) -> bool {
 pub fn testar_postgres(porta: u16) -> bool {
     // SELECT 1 é neutro, só testa se aceita comando
     let args = [
-        "-h", "12.0.0.1",
+        "-h", "127.0.0.1",
         "-p", &porta.to_string(),
         "-U", "postgres",
         "-t",
@@ -34,19 +34,35 @@ pub fn testar_postgres(porta: u16) -> bool {
     }
 }
 
+pub fn testar_mysql(porta: u16) -> bool {
+    let args = [
+        "-h", "127.0.0.1",
+        "-P", &porta.to_string(),
+        "-u", "root",
+        "-e", "SELECT 1",
+    ];
+
+    let output = Command::new("mysql").args(&args).output();
+
+    match output {
+        Ok(o) => o.status.success(),
+        Err(_) => false, 
+    }
+}
+
 pub fn buscar_portas_automatico() -> Vec<ResultadoPorta> {
-    let portas = [5432, 6433, 3306];
+    let portas = [5432, 5433, 3306];
     let mut resultados = Vec::new();
 
     for porta in portas{
         if porta_aberta(porta){
-
+        
             let mut r = ResultadoPorta { 
                 porta,
                 info: InfoServico::ServicoInexistente,
             };
 
-            if porta == 5432 && testar_postgres(porta) {
+            if porta == 5432 || porta == 5433  && testar_postgres(porta) {
                 r.info = InfoServico::PostgreSQL;
             }
 
